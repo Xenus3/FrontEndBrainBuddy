d6e1bade726fc15617ff23f6a0b9e9ee3a709f86
VAUBAN_C5\Administrateur <k.aberbach@hotmail.com>
2023-06-22 16:39:43 +0200

    menu_burger_2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
