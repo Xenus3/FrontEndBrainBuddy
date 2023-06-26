b7f416fd58290399f853eed0813546abc042a81d
VAUBAN_C2\Administrateur <bounouhconseils@gmail.com>
2023-06-26 10:06:28 +0200

    les dernier modification de footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AccueilComponent } from './components/accueil/accueil.component';
-<<<<<<< HEAD
 import { FooterComponent } from './components/footer/footer.component';
 import { ReactiveFormsModule } from '@angular/forms';
-=======
 import { ConnexionComponent } from './components/connexion/connexion.component';
-import { ReactiveFormsModule } from '@angular/forms';
->>>>>>> 6248bcd7f9ac3895f2e6fcdca1ab49959beb24c1
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AccueilComponent,
-<<<<<<< HEAD
-    FooterComponent
-=======
+    FooterComponent,
     ConnexionComponent
->>>>>>> 6248bcd7f9ac3895f2e6fcdca1ab49959beb24c1
   ],
   imports: [
     BrowserModule,
