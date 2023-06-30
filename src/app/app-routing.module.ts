f5e938775bcf8f5abd42f8eebbdee4482b2e7688
unknown <Adrien>
2023-06-30 16:23:07 +0200

    updated path of monkeytest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     component: AccueilComponent
   },
   {
-    path: "app-monkey-test-tutoriel",
+    path: "monkeytest",
     component: MonkeyTestTutorielComponent
   },
   {
