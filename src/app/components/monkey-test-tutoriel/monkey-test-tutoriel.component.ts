9ef5b3ea33f62e504c13f325cdb10432d9deac03
Adrien <Adrien>
2023-07-04 12:06:48 +0200

    slider workds without bubbles yet

--- src/app/components/monkey-test-tutoriel/monkey-test-tutoriel.component.ts
+++ src/app/components/monkey-test-tutoriel/monkey-test-tutoriel.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./monkey-test-tutoriel.component.scss']
 })
 export class MonkeyTestTutorielComponent {
+  // const triangleGauche = document.getElementById('triangle-gauche') as HTMLElement;
 
+  slideRight(): void {
+    const slide1 = document.getElementById('slide1') as HTMLElement;
+    const slide2 = document.getElementById('slide2') as HTMLElement;
+    const slide3 = document.getElementById('slide3') as HTMLElement;
+    if (slide1.hidden === false) {
+      slide2.hidden = false;
+      slide3.hidden = true;
+      slide1.hidden = true;
+      } else if (slide2.hidden === false){
+        slide3.hidden = false;
+        slide1.hidden = true;
+        slide2.hidden = true;
+        
+      } else if (slide3.hidden === false) {
+        slide1.hidden = false;
+        slide2.hidden = true;
+        slide3.hidden = true;
+      };
+    }
+  slideLeft(): void {
+    const slide1 = document.getElementById('slide1') as HTMLElement;
+    const slide2 = document.getElementById('slide2') as HTMLElement;
+    const slide3 = document.getElementById('slide3') as HTMLElement;
+    if (slide1.hidden === false) {
+      slide3.hidden = false;
+      slide2.hidden = true;
+      slide1.hidden = true;
+      } else if (slide3.hidden === false){
+        slide2.hidden = false;
+        slide1.hidden = true;
+        slide3.hidden = true;
+        
+      } else if (slide2.hidden === false) {
+        slide1.hidden = false;
+        slide3.hidden = true;
+        slide2.hidden = true;
+      };
+    }
 }
