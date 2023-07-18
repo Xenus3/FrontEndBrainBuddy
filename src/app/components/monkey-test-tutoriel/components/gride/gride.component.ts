06baeeb04a067c40600a39bbd3cb68ba52164a20
AdrienDSlone <amh.marceau@gmail.com>
2023-07-18 16:08:37 +0200

    grid-system started

--- src/app/components/monkey-test-tutoriel/components/gride/gride.component.ts
+++ src/app/components/monkey-test-tutoriel/components/gride/gride.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class GrideComponent {
 
+  let container = document.getElementById("container");
+  
+  ngOnInit(): void {
+  }
+
+  makeRows(rows: number, cols: number): void {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (i = 0; i < (rows * cols); i++) {
+      let cell = document.createElement("div");
+      cell.innerText = (i + 1);
+      container.appendChild(cell).className = "grid-item" + i;
+    };
+  };
+
 }
