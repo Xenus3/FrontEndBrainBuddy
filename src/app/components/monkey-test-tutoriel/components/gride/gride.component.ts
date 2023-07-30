01040ddd9519607eccaefbddadd14a8b683fb51e
adrien marceau <amh.marceau@gmail.com>
2023-07-30 20:10:34 +0200

    solved load of the div when page is loaded ie without a onclick on a button as previously

--- src/app/components/monkey-test-tutoriel/components/gride/gride.component.ts
+++ src/app/components/monkey-test-tutoriel/components/gride/gride.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-gride',
@@ -14,17 +14,23 @@ export class GrideComponent {
 
   makeRows(rows: string, cols: string): void {
     const container = document.getElementById('container') as HTMLElement;
+    const btnjouer = document.getElementById('btn-jouer') as HTMLElement;
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     var i : number;
     var y: number = +cols;
     var x: number = +rows;
-
+    
     for (i = 1; i <= y*x; i++) {
       console.log(y*x);
       let cell = document.createElement("div") as HTMLElement;
       cell.innerText = (""+i+"");
       container.appendChild(cell).className = "tirage " + i;
-      }
+    }
     };
+
+    ngAfterViewInit(){
+      this.makeRows('10','15');;//Put here your function or what you need
+    }
+
   };
