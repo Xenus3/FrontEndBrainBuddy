192e8e4c05c4838a5e3689f12d06b7ea0246fcc7
AdrienDSlone <amh.marceau@gmail.com>
2023-07-31 16:55:05 +0200

    wip : grille display random draw and not the null elements in the array grille, thanks to Jérome strategy

--- src/app/components/monkey-test/components/jeu/components/grille/grille.component.ts
+++ src/app/components/monkey-test/components/jeu/components/grille/grille.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-grille',
   templateUrl: './grille.component.html',
   styleUrls: ['./grille.component.scss']
 })
-export class GrilleComponent {
+
+export class GrilleComponent implements OnInit {
+
+  // Variables
+  life = 3;
+  level = 4;
+  rows = 5;
+  columns = 10;
+  draw = this.rows * this.columns;
+  container = document.getElementById("tirage");
+
+  grille = new Array(this.draw);
+
+  // Lancement du jeu
+  ngOnInit() {
+    this.generateGrid();
+  }
+
+  generateGrid() {
+    let tirage : number;
+    for (let i = 0; i < this.draw; i++) {
+      this.grille[i] = null;
+    }
+    let x : number = 1;
+    for (let i = 0; i < this.level; i++){
+      tirage = this.randomIntFromInterval();
+      console.log(tirage);
+      this.grille[tirage - 1] =  x;
+      x++;
+    }
+  }
+
+  // Génére un nombre aléatoire compris entre 1 et la taille de la grille (this.draw) du jeu 
+  randomIntFromInterval() {
+    return Math.floor(Math.random() * this.draw + 1)
+  }
 
 }
