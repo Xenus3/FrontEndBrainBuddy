67e893d2e703294738fb25d82358e0534a1d01a4
AdrienDSlone <amh.marceau@gmail.com>
2023-07-31 11:47:23 +0200

    wip grid-system : trying to display number on a line, also started to create a random draw

--- src/app/components/monkey-test-tutoriel/components/gride/gride.component.ts
+++ src/app/components/monkey-test-tutoriel/components/gride/gride.component.ts
@@ -7,13 +7,31 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class GrideComponent {
 
-  container = document.getElementById("container");
+  container = document.getElementById("tirage");
   
+  // Lancement du jeu
   ngOnInit(): void {
+    let life = 3;
+    let level = 4;
+    while (life > 0) {
+      this.tirage(level);
+      this.makeRows('5','8');
+    }
   }
 
+  // Tirage de l'emplacement des n cartes à deviner (n étant le level)
+  tirage(level:number){
+    let cells : number[];
+  }
+
+  // Générer des nombres aléatoires compris dans la taille de la grille
+  randomIntFromInterval() {
+    return Math.floor(Math.random() * (40 - 1 + 1) + 1)
+  }
+
+  // Créer la grille
   makeRows(rows: string, cols: string): void {
-    const container = document.getElementById('container') as HTMLElement;
+    const container = document.getElementById('tirage') as HTMLElement;
     const btnjouer = document.getElementById('btn-jouer') as HTMLElement;
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
@@ -25,13 +43,13 @@ export class GrideComponent {
       console.log(y*x);
       let cell = document.createElement("div") as HTMLElement;
       cell.innerText = (""+i+"");
-      container.appendChild(cell).className = "tirage";
-      //container.appendChild(cell).className = "tirage " + i+"";
-    }
+      cell.className = "tirage " + i;
+      container.appendChild(cell);
+      }
     };
 
-    ngAfterViewInit(){
-      this.makeRows('8','5');;//Put here your function or what you need
-    }
+  // ngAfterViewInit(){
+  //   ;//Put here your function or what you need
+  // }
 
   };
