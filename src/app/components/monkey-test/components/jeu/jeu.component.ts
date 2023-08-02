40b4115ad0f59590e2586b4f62d17352f8b23436
AdrienDSlone <amh.marceau@gmail.com>
2023-08-02 10:22:26 +0200

    UI monkeyTest game : icones added

--- src/app/components/monkey-test/components/jeu/jeu.component.ts
+++ src/app/components/monkey-test/components/jeu/jeu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { GrilleComponent } from './components/grille/grille.component';
 
 @Component({
   selector: 'app-jeu',
@@ -6,5 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jeu.component.scss']
 })
 
-export class JeuComponent {
+export class JeuComponent implements AfterViewInit {
+  @ViewChild(GrilleComponent) grilleComponentLife: any
+  life : any;
+
+  @ViewChild(GrilleComponent) grilleComponentLevel: any
+  level : any;
+
+  ngAfterViewInit() {
+    this.life = this.grilleComponentLife.life;
+    this.level = this.grilleComponentLevel.level;
+  }
+
 }
